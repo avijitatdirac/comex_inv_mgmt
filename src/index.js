f4f7fa78e907f57ce494d456b115b6c470f6519d
Sibaprasad Maiti <sibaprasad@Users-MacBook-Pro.local>
2018-07-15 20:07:00 +0530

    check_login API added

--- src/index.js
+++ src/index.js
@@ -2,35 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from './Login';
-// import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { history } from './_helpers';
-const fakeAuth = {
-	isAuthenticated: false,
-	authenticate(cb) {
-		this.isAuthenticated = true;
-		setTimeout(cb, 100); // fake async
-	},
-	signout(cb) {
-		this.isAuthenticated = false;
-		setTimeout(cb, 100);
-	},
-};
-
-function LandingPage(props) {
-	const isLoggedIn = props.isLoggedIn;
-	if (isLoggedIn) {
-		return <App />;
-	}
-	return <Login />;
-}
-
-/*ReactDOM.render(
-	// Try changing to isLoggedIn={true}:
-	<LandingPage isLoggedIn={false} />,
-	document.getElementById('root')
-);*/
 
 ReactDOM.render(
 	<BrowserRouter>
@@ -38,18 +10,4 @@ ReactDOM.render(
 	</BrowserRouter>,
 	document.getElementById('root')
 );
-// registerServiceWorker();
-/*(function(seconds) {
-    var refresh,       
-        intvrefresh = function() {
-			
-            clearInterval(refresh);
-            refresh = setTimeout(function() {
-              history.push('/login')
-            }, seconds * 1000);
-        };
 
-    window.document.addEventListener('keypress click', function() { intvrefresh() });
-    intvrefresh();
-
-}(30));*/
